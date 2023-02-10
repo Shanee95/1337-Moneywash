fe863273e66d4b92e9a5fe84bdc28bbb77a220b1
Shane <42964189+Shanee95@users.noreply.github.com>
2023-02-10 11:16:38 +0000

    files

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config.PoliceJob = 'police'
+Config.RequiredCops = 0
+
+Config.MetaDataName = 'moneywashed'
+
+Config.Dirtymoney = 'dirtymoney'
+Config.BaseReturn = 50
+Config.LevelReturn = 1
+
+Config.Experience = {
+    2500,
+    5000,
+    7500,
+    10000,
+    15000,
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Shane'
+description 'Money washing for 1337 City'
+version '1.0'
+
+shared_script '@qb-core/shared/items.lua'
+server_script 'server/main.lua'
+client_script 'client/main.lua'
